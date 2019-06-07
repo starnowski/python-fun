11f3acf3612dd6182983f25015317f925be0a2d9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-06-08 01:28:31 +0200

    Added link which shows "Few ways how to do string concatenation in python"

--- fundamentals/validStringConcatenationBecauseTypesMatchesAfterCasting.py
+++ fundamentals/validStringConcatenationBecauseTypesMatchesAfterCasting.py
@@ -4,3 +4,6 @@
 #TODO Add unit test which execute python from python and check python interperter output
 
 print("some string " + str(12))
+
+# Few ways how to do string concatenation in python
+# https://www.jquery-az.com/5-ways-python-string-concatenation-6-examples/
