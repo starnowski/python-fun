ae67533f9078369e4dd93d0153ce5b8a31a44621
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-06-13 00:25:41 +0200

    Added examples of functions

---
+++ fundamentals/runFunctionWithTwoParameters.py
@@ -0,0 +1,9 @@
+
+
+def func(a, b=2):
+    return a * 2 + b
+
+print("1. Function has " + str(func(1, 2)))
+print("2. Function has " + str(func(2, 1)))
+
+print("3. DIfferent bag" + str(func(2)))
---
+++ fundamentals/runFunctionWithTwoParametersButWithOpositeOrderForInvocationMethond.py
@@ -0,0 +1,9 @@
+
+
+def func(a, b):
+    return a * 2 + b
+
+print("1. Function has " + str(func(1, 2)))
+print("2. Function has " + str(func(2, 1)))
+
+print("3. DIfferent bag" + str(func(b=2, a=1)))
