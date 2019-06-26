0c337d39bcb22f31d6d39d56be1bbc5bb109516e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-06-27 01:32:37 +0200

    Added type inheritance example for simple class in Python

---
+++ fundamentals/fundamentals/oop/InheritatedClassExample.py
@@ -0,0 +1,34 @@
+class SimpleClass:
+
+    def __init__(self):
+        pass  # constructor?
+
+    def print_hello(self):
+        print "Hello from class 'SimpleClass' : " + str(self)
+
+    @staticmethod
+    def print_hello_by_class():
+        print "Hello from static method for class 'SimpleClass'"
+
+    def print_hello_to_someone(self, person_name):
+        print "Hello says " + str(self) + " to " + person_name
+
+
+class InheritatedClassExample(SimpleClass):
+    def print_hello(self):
+        SimpleClass.print_hello(self)
+        print "Hello from class 'InheritatedClassExample' : " + str(self)
+
+def main():
+    ob = SimpleClass()  # initializing object of type simpleClass
+    ob.print_hello()
+    SimpleClass.print_hello_by_class()
+    ob.print_hello_to_someone("Szymon")
+
+    ice = InheritatedClassExample()
+    ice.print_hello()
+    ice.print_hello_to_someone("Szymon")
+
+
+if __name__ == "__main__":
+    main()
