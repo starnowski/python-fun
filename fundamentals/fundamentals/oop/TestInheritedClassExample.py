fa2d24e3630a0a6c375cbfa8e4be1dc718ab37eb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-07-01 00:55:51 +0200

    Added second test case

--- fundamentals/fundamentals/oop/TestInheritedClassExample.py
+++ fundamentals/fundamentals/oop/TestInheritedClassExample.py
@@ -1,5 +1,6 @@
 import unittest
 from InheritedClassExample import InheritedClassExample
+from InheritedClassExample import SimpleClass
 
 
 class TestStringMethods(unittest.TestCase):
@@ -7,6 +8,11 @@ class TestStringMethods(unittest.TestCase):
     def test_should_return_string_with_suffix_InheritedClassExample(self):
         tested = InheritedClassExample()
         self.assertEqual("The object of class InheritedClassExample welcomes you, Szymon",
+                         tested.return_welcome_statement("Szymon"))
+
+    def test_should_return_string_with_suffix_SimpleClass(self):
+        tested = SimpleClass()
+        self.assertEqual("The object of class SimpleClass welcomes you, Szymon",
                          tested.return_welcome_statement("Szymon"))
 
 
