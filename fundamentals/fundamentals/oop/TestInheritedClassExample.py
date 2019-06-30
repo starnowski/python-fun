d384a5337045163d790353639a8bdf904facd1ee
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-07-01 00:31:26 +0200

    Added first unit test

---
+++ fundamentals/fundamentals/oop/TestInheritedClassExample.py
@@ -0,0 +1,13 @@
+import unittest
+from InheritedClassExample import InheritedClassExample
+
+class TestStringMethods(unittest.TestCase):
+
+
+    def test_should_return_string_with_suffix_InheritedClassExample(self):
+        tested = InheritedClassExample()
+        self.assertEqual("The object of class InheritedClassExample welcomes you, Szymon", tested.return_weclome_statement("Szymon"))
+
+
+if __name__ == '__main__':
+    unittest.main()
