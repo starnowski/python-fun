854aca4925bcaddea45df54a388ce466425c0b42
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-07-08 23:26:27 +0200

    Fixed test case based on default printed time delta format

--- fundamentals/fundamentals/timedelta/test_simple_timedelta_provider.py
+++ fundamentals/fundamentals/timedelta/test_simple_timedelta_provider.py
@@ -6,7 +6,7 @@ from simple_timedelta_provider import SimpleTimedeltaProvider
 class TestStringMethods(unittest.TestCase):
 
     def test_should_return_correct_string_for_year_delta_time(self):
-        self.assertEqual("365 days",
+        self.assertEqual("365 days, 0:00:00",
                          SimpleTimedeltaProvider.get_year_delta_string())
 
 
