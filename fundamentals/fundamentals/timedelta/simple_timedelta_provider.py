a57be5bd3f22c6f818641a598f53282be174153b
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-07-08 23:23:39 +0200

    Added test case which checks if correct string was returned for year delta time

---
+++ fundamentals/fundamentals/timedelta/simple_timedelta_provider.py
@@ -0,0 +1,9 @@
+# simple_timedelta_provider.py
+class SimpleTimedeltaProvider:
+
+    def __init__(self):
+        pass  # constructor?
+
+    @staticmethod
+    def get_year_delta_string():
+        return ""
---
+++ fundamentals/fundamentals/timedelta/test_simple_timedelta_provider.py
@@ -0,0 +1,14 @@
+import unittest
+
+from simple_timedelta_provider import SimpleTimedeltaProvider
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_should_return_correct_string_for_year_delta_time(self):
+        self.assertEqual("365 days",
+                         SimpleTimedeltaProvider.get_year_delta_string())
+
+
+if __name__ == '__main__':
+    unittest.main()
