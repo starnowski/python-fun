e35910a7bfa566772fd33434597e0f891e6b0ee3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-07-10 01:47:34 +0200

    Added test case for appending content to file

--- fundamentals/fundamentals/files/test_read_file.py
+++ fundamentals/fundamentals/files/test_read_file.py
@@ -22,6 +22,32 @@ class TestReadFile(unittest.TestCase):
         self.assertEqual("first line\n", fl[0])
         self.assertEqual("second line", fl[1])
 
+    def test_should_create_new_file_with_content_and_then_append_it(self):
+        # given
+        file_number = random.randint(1, 99999)
+
+        # when
+        f = open(str(file_number) + "_write_test.txt", "w+")
+        self.f = f
+        f.write("first line\n")
+        f.write("second line")
+        f.close()
+
+        f = open(str(file_number) + "_write_test.txt", "a")
+        self.f = f
+        f.write("\nthird line\n")
+        f.write("fourth line")
+        f.close()
+
+        # then
+        f = open(str(file_number) + "_write_test.txt", "r")
+        self.f = f
+        fl = f.readlines()
+        self.assertEqual("first line\n", fl[0])
+        self.assertEqual("second line\n", fl[1])
+        self.assertEqual("third line\n", fl[2])
+        self.assertEqual("fourth line", fl[3])
+
     def test_should_return_all_lines_in_file(self):
         f = open("test1.txt", "r")
         self.f = f
