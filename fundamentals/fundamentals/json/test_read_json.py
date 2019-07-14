1d8e31eb168e5e30ae109dbf0db55f6c4a9bffaf
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-07-15 01:32:16 +0200

    Added assertions which checks the content of githubRepositories property

--- fundamentals/fundamentals/json/test_read_json.py
+++ fundamentals/fundamentals/json/test_read_json.py
@@ -16,3 +16,8 @@ class TestReadJson(unittest.TestCase):
             self.assertTrue("firstName" in json_data, "Json should contains property \"firstName\"")
             self.assertTrue("lastName" in json_data, "Json should contains property \"lastName\"")
             self.assertTrue("githubRepositories" in json_data, "Json should contains property \"githubRepositories\"")
+            self.assertEqual(4, json_data["githubRepositories"].__len__(), "The array \"githubRepositories\" should contains four objects")
+            self.assertEqual("docker-fun", json_data["githubRepositories"][0]["name"], "First mentioned repository should be \"docker-fun\"")
+            self.assertEqual("posmulten", json_data["githubRepositories"][1]["name"], "Second mentioned repository should be \"posmulten\"")
+            self.assertEqual("bmunit-extension", json_data["githubRepositories"][2]["name"], "Third mentioned repository should be \"bmunit-extension\"")
+            self.assertEqual("bash-fun", json_data["githubRepositories"][3]["name"], "Fourth mentioned repository should be \"bash-fun\"")
