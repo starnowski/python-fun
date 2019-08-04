e90625762386225cd022ffd803e1a1c20e1e9ed4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-08-04 12:29:16 +0200

    #13 Added YamlFileHelper class (not yet implemented) [skip travis]

---
+++ yamale-fun/validators/yaml_utils.py
@@ -0,0 +1,13 @@
+import yaml
+
+class YamlFileHelper:
+
+    yaml = None
+
+    def __int__(self, filepath):
+        # TODO
+        pass
+
+    def contains_jsonpath(self, jsonpath):
+        # TODO
+        return False
