f96b4ca6c08aeea849c8a3e28627c63d541bf68a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-08-05 21:07:06 +0200

    #13 Changed local properties to object properties [skip travis]

--- yamale-fun/validators/test_yaml_utils.py
+++ yamale-fun/validators/test_yaml_utils.py
@@ -6,19 +6,17 @@ from validators.yaml_utils import YamlFileHelper
 
 class YamlFileHelperTest(unittest.TestCase):
 
-    # test_file = "test/yaml_utils/data.yaml"
-    # tested = YamlFileHelper(test_file)
+    test_file = "test/yaml_utils/data.yaml"
+    tested = YamlFileHelper(test_file)
 
     def test_should_return_true_for_existed_main_root_node(self):
         # given
-        test_file = "test/yaml_utils/data.yaml"
-        tested = YamlFileHelper(test_file)
-        with open(test_file, "r") as f:
+        with open(self.test_file, "r") as f:
             yaml_data = load(f)
             self.assertTrue("organization" in yaml_data, "Json should contains property \"name\"")
 
         # when
-        result = tested.contains_jsonpath("organization")
+        result = self.tested.contains_jsonpath("organization")
 
         # then
         self.assertTrue(result, "YamlFileHelper object should return true that \"organization\" node exists!")
