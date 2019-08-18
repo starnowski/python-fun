80607462fad28bbaaaecdc71cf896493e9f0c4cb
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-08-18 10:10:51 +0200

    #2 - Fixed 'tag' option [skip travis]

--- yamale-fun/validators/src/required_reference_validator.py
+++ yamale-fun/validators/src/required_reference_validator.py
@@ -3,7 +3,7 @@ from yamale.validators import DefaultValidators, Validator
 
 class RequiredReferenceValidator(Validator):
     """ Custom Date validator """
-    tag = 'date'
+    tag = 'ref_req'
 
     def _is_valid(self, value):
         return isinstance(value, datetime.date)
