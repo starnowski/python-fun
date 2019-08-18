db9d16e4ffe3ff906bbfbd01c5986d7817a5a13e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-08-18 22:36:42 +0200

    #2 - Added test cases for valid data  [skip travis]

--- yamale-fun/validators/tests/test_required_reference_validator.py
+++ yamale-fun/validators/tests/test_required_reference_validator.py
@@ -31,6 +31,36 @@ class TestRequiredReferenceValidator(unittest.TestCase):
             # then
             pass
 
+    def test_should_return_true_for_valid_document_with_all_properties(self):
+        # given
+        tested = YamaleRequiredReferenceFacade(self.schema_file)
+        test_file = path.join(os.path.dirname(__file__), "required_reference_validator/valid_data/passenger_seat/passenger_with_infant.yaml")
+        with open(test_file, "r") as f:
+            yaml_data = yaml.load(f)
+            self.assertTrue("passenger_infant_name" in yaml_data, "Yaml should contains property \"passenger_infant_name\"")
+            self.assertTrue("passenger_name" in yaml_data, "Yaml should contains property \"passenger_name\"")
+
+        # when
+        result = tested.validate(test_file)
+
+        # then
+        self.assertTrue(result, "Validator should return true")
+
+    def test_should_return_true_for_valid_document_without_optional_property(self):
+        # given
+        tested = YamaleRequiredReferenceFacade(self.schema_file)
+        test_file = path.join(os.path.dirname(__file__), "required_reference_validator/valid_data/passenger_seat/passenger_without_infant.yaml")
+        with open(test_file, "r") as f:
+            yaml_data = yaml.load(f)
+            self.assertFalse("passenger_infant_name" in yaml_data, "Yaml should not contains property \"passenger_infant_name\" which is optional property")
+            self.assertTrue("passenger_name" in yaml_data, "Yaml should contains property \"passenger_name\"")
+
+        # when
+        result = tested.validate(test_file)
+
+        # then
+        self.assertTrue(result, "Validator should return true")
+
 
 if __name__ == '__main__':
     unittest.main()
