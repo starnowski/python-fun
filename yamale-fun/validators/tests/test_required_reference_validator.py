df3bdeb4003809ea08e778d2d1a19d9ee0aa245a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-08-19 00:04:42 +0200

    #2 - Added test case with empty passenger name and non empty infant [skip travis]

--- yamale-fun/validators/tests/test_required_reference_validator.py
+++ yamale-fun/validators/tests/test_required_reference_validator.py
@@ -32,6 +32,26 @@ class TestRequiredReferenceValidator(unittest.TestCase):
             # then
             pass
 
+    def test_should_return_validation_error_for_invalid_schema(self):
+        # given
+        tested = YamaleRequiredReferenceFacade(self.schema_file)
+        test_file = path.join(os.path.dirname(__file__), "required_reference_validator/invalid_data/passenger_seat/infant_with_empty_passenger.yaml")
+        with open(test_file, "r") as f:
+            yaml_data = yaml.load(f)
+            self.assertTrue("passenger_infant_name" in yaml_data, "Yaml should contains property \"passenger_infant_name\"")
+            self.assertEqual("Mark Doe", yaml_data.get("passenger_infant_name"), "The element \"passenger_infant_name\" should have value \"Mark Doe\"")
+            self.assertTrue("passenger_name" in yaml_data, "Yaml should not contains property \"passenger_name\"")
+            self.assertEqual(None, yaml_data.get("passenger_name"), "The element \"passenger_name\" should have empty value")
+
+        try:
+            # when
+            tested.validate(test_file)
+            self.fail("Expected ValueError")
+
+        except ValueError:
+            # then
+            pass
+
     def test_should_return_true_for_valid_document_with_all_properties(self):
         # given
         tested = YamaleRequiredReferenceFacade(self.schema_file)
