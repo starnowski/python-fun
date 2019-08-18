773e01ab3941afe3092848289c5afc7558e161d6
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2019-08-18 19:40:25 +0200

    #2 - Changed test case [skip travis]

--- yamale-fun/validators/tests/test_required_reference_validator.py
+++ yamale-fun/validators/tests/test_required_reference_validator.py
@@ -6,7 +6,7 @@ import itertools
 import yamale
 from yamale import validators
 
-from validators.src.required_reference_validator import RequiredReferenceValidator
+from validators.src.required_reference_validator import RequiredReferenceValidator, YamaleRequiredReferenceFacade
 
 
 class TestRequiredReferenceValidator(unittest.TestCase):
@@ -15,21 +15,16 @@ class TestRequiredReferenceValidator(unittest.TestCase):
 
     def test_should_return_validation_error_for_invalid_schema(self):
         # given
+        tested = YamaleRequiredReferenceFacade(self.schema_file)
         test_file = path.join(os.path.dirname(__file__), "required_reference_validator/invalid_data/passenger_seat/infant_without_passenger.yaml")
         with open(test_file, "r") as f:
             yaml_data = yaml.load(f)
             self.assertTrue("passenger_infant_name" in yaml_data, "Yaml should contains property \"passenger_infant_name\"")
             self.assertFalse("passenger_name" in yaml_data, "Yaml should not contains property \"passenger_name\"")
 
-        # required_reference_validator = RequiredReferenceValidator(test_file)
-            validators_list = validators.DefaultValidators
-            validators_list[RequiredReferenceValidator.__name__] = RequiredReferenceValidator
-            validators_list[RequiredReferenceValidator.tag] = RequiredReferenceValidator
-            yamale_schema = yamale.make_schema(self.schema_file, validators=validators_list)
-            yamale_data = itertools.chain(*map(yamale.make_data, [test_file]))
-
         # when
-            result = yamale.validate(yamale_schema, yamale_data) is not None
+        result = tested.validate(test_file)
+
         ### TODO
 
 
